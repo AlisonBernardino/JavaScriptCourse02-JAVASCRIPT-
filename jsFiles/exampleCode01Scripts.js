8068c2773d5c610c631cd9ba15681821aedd2b51
Alison Bernardino <58011286+AlisonBernardino@users.noreply.github.com>
2021-06-04 02:07:57 -0300

    codeExample01 added

---
+++ jsFiles/exampleCode01Scripts.js
@@ -0,0 +1,16 @@
+// alert('All set!');
+/*
+    Objectives = Capture the username and show a custom "Welcome" message.
+    Define the vars with .js
+    Input elements = Prompt method from window object
+    Output elements = Write method from document object
+*/
+    // Vars definition =
+    var jsName;
+
+    // Data insertion =
+    jsName = window.prompt('Insert your name = ','Use this box to write your name');
+
+    // Data output =
+    document.write('Hello, ' + jsName + '. Welcome aboard =) ');
+    // This line 15 code shows a custom "Hi" message
