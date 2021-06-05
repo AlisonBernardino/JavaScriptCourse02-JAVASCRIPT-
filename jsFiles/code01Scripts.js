ad62503fe9ea6870b6de3eef0ac79986d062598f
Alison Bernardino <58011286+AlisonBernardino@users.noreply.github.com>
2021-06-05 03:57:22 -0300

    code01Exercise added

---
+++ jsFiles/code01Scripts.js
@@ -0,0 +1,7 @@
+// alert('All set!');
+
+var carFuelStatus;
+
+fuelCondition = window.prompt('Plase, insert the car fuel condition = ', 'Use this box to insert data...');
+
+document.write('Your car fuel condition is = ' + fuelCondition + '. You have 25% of tank backup. Travel safe!');
